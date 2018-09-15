4de33eaac9cb565abbe1d31021e24578b91eedd5
Louis <louismc4@hotmail.com>
2018-09-15 13:14:16 -0400

    change back to spawn

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ wss.on('connection', (ws) => {
   console.log('Target RTMP URL:', rtmpUrl);
   
   // Launch FFmpeg to handle all appropriate transcoding, muxing, and RTMP
-  const ffmpeg = child_process.exec('ffmpeg', [
+  const ffmpeg = child_process.spawn('ffmpeg', [
     // Facebook requires an audio track, so we create a silent one here.
     // Remove this line, as well as `-shortest`, if you send audio from the browser.
     '-f', 'lavfi', '-i', 'anullsrc',
