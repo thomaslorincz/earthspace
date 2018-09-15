6bef1ced230ca859514ee84039f3ca0a131bdd06
Thomas Lorincz <tlorincz@ualberta.ca>
2018-09-15 03:18:31 -0400

    Added ability to drag and drop single CSV file into panel

--- src/index.js
+++ src/index.js
@@ -12,9 +12,6 @@ var SCREEN_WIDTH = window.innerWidth,
 
 var earthMesh;
 
-init();
-animate();
-
 function init() {
 
   var container;
@@ -81,3 +78,63 @@ function render() {
   renderer.clear();
   renderer.render(scene, camera);
 }
+
+init();
+animate();
+
+document.getElementById('panel').ondragover = (e) => {
+  e.preventDefault();
+  document.getElementById('panel').classList.add('dragOver');
+};
+
+document.getElementById('panel').ondragleave = (e) => {
+  e.preventDefault();
+  document.getElementById('panel').classList.remove('dragOver');
+};
+
+document.getElementById('panel').ondrop = (e) => {
+  e.preventDefault();
+
+  if (e.dataTransfer.items) {
+    // Use DataTransferItemList interface to access the file(s)
+    for (let i = 0; i < e.dataTransfer.items.length; i++) {
+      // If dropped items aren't files, reject them
+      if (e.dataTransfer.items[i].kind === 'file') {
+        const file = e.dataTransfer.items[i].getAsFile();
+        const reader = new FileReader();
+        reader.readAsText(file);
+        reader.onload = (e) => {
+          const table = document.createElement('table');
+          table.setAttribute('id', 'table');
+          document.getElementById('panel').appendChild(table);
+          let csvData = e.target.result;
+          csvData = csvData.replace(/"/g, '');
+          const rows = csvData.split('\n');
+          for (let j = 0; j < rows.length; j++) {
+            const row = rows[j].split(',');
+            const tableRow = document.createElement('tr')
+            table.appendChild(tableRow);
+            for (let k = 0; k < row.length; k++) {
+              const tableCell = document.createElement('td');
+              tableCell.innerText = row[k];
+              tableRow.appendChild(tableCell);
+            }
+          }
+        };
+      }
+    }
+  } else {
+    // Use DataTransfer interface to access the file(s)
+    for (let i = 0; i < e.dataTransfer.files.length; i++) {
+      console.log('... file[' + i + '].name = ' + e.dataTransfer.files[i].name);
+    }
+  }
+
+  if (e.dataTransfer.items) {
+    // Use DataTransferItemList interface to remove the drag data
+    e.dataTransfer.items.clear();
+  } else {
+    // Use DataTransfer interface to remove the drag data
+    e.dataTransfer.clearData();
+  }
+};
